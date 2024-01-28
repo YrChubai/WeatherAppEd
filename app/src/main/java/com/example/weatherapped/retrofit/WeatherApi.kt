73ab83543f9e73baa0e908248dc60f153e162610
Chubai <razawchs@gmail.com>
2024-01-28 10:55:50 +0300

    Update WeatherApi

--- app/src/main/java/com/example/weatherapped/retrofit/WeatherApi.kt
+++ app/src/main/java/com/example/weatherapped/retrofit/WeatherApi.kt
@@ -1,4 +1,5 @@
 package com.example.weatherapped.retrofit
+import com.example.weatherapped.adapters.WeatherModel
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -6,12 +7,12 @@ import retrofit2.http.Query
 interface WeatherApi {
     interface WeatherApi {
         @GET("forecast.json")
-        fun getWeatherData(
+        suspend fun getWeatherData(
             @Query("key") apiKey: String,
             @Query("q") city: String,
             @Query("days") days: String,
             @Query("aqi") aqi: String,
             @Query("alerts") alerts: String
-        ): Call<String>
+        ): WeatherDataModel
     }
 }
---
+++ app/src/main/java/com/example/weatherapped/retrofit/WeatherDataModel.kt
@@ -0,0 +1,24 @@
+package com.example.weatherapped.retrofit
+
+data class WeatherDataModel(
+    val location: LocalModel,
+    val current: CurrentModel,
+    val forecast: ForecastModel
+)
+
+data class LocalModel(
+    val name: String,
+    val localtime: String
+)
+
+data class CurrentModel(
+    val last_updated: String,
+    val temp_c: Float,
+    val condition: ConditionModel
+)
+
+data class ConditionModel(
+    val text: String,
+    val icon: String
+)
+
